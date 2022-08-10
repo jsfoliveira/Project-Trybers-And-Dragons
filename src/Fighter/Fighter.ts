f3f6af05bf7ae4204dcc4d0fbfecef797cc8392f
Juliana Oliveira <jsfoliveira89@gmail.com>
2022-08-10 14:56:35 -0300

    Requisito 6: Criada a interface Fighter

---
+++ src/Fighter/Fighter.ts
@@ -0,0 +1,18 @@
+import Energy from '../Energy';
+
+// A interface Fighter existe e está sendo exportada
+export default interface Fighter {
+  // A interface Fighter possui os atributos lifePoints lifePoints, strength, defense, energy
+  lifePoints: number;
+  strength: number;
+  defense: number;
+  energy?:Energy;
+  // A interface Fighter possui o método attack(), que recebe um enemy do tipo Fighter
+  attack(enemy: Fighter): void;
+  // A interface Fighter possui o método special(), que recebe um enemy do tipo Fighter
+  special(enemy: Fighter): void;
+  // A interface Fighter possui o método levelUp(), que não recebe parâmetros nem retorna nada
+  levelUp(): void;
+  // A interface Fighter possui o método receiveDamage(), que recebe um attackPoints do tipo number
+  receiveDamage(attackPoints: number): number;
+}
